d7c758941d6e79a3e14503dc47f379b4b251ba9b
Andrew Savinykh <andrewsav@datacom.co.nz>
2018-01-05 10:38:32 +1300

    incorporating changes from https://codereview.stackexchange.com/a/184163/58154

--- docker/vault-unseal.py
+++ docker/vault-unseal.py
@@ -2,120 +2,108 @@ import os
 import sys
 import json
 import time
-import datetime
-from pprint import pprint
+import logging
+from pprint import pformat
+
 import requests
 
-version = "0.1"
-envPrefix = "VU_"
-config = "vault-unseal.json"
 
-def PrintWithTimestamp(string):
-  timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-  sys.stdout.write(f"[{timestamp}] {string}\n")
+VERSION = '0.1'
+ENV_PREFIX = 'VU_'
+CONFIG_FILENAME = 'vault-unseal.json'
 
-def PrintDebug(string):
-  if "VU_DEBUG" in os.environ:
-    PrintWithTimestamp(f"DEBUG: {string}")
 
-def PrintParameterError(name):
-  PrintWithTimestamp(f"Error: {name} is not specified. Specify it either in {config} file, or as enviroment variable prefixed with {envPrefix}")
+def configure_logger():
+    logging.basicConfig(stream=sys.stdout, format='[%(asctime)s] %(levelname)s:%(message)s')
+    logger = logging.getLogger('vault-unseal')
+    logger.setLevel(logging.DEBUG if 'VU_DEBUG' in os.environ else logging.INFO)
+    return logger
 
-def NormalizePrefix(name, string):
-  return name + string[len(envPrefix+name):]
 
-def ReadSingleSetting(name):
-  result = None
-  try:
-    with open(config) as data_file:    
-      data = json.load(data_file)
-    result = data[name]
-  except (FileNotFoundError, KeyError):
-    pass
-  if envPrefix+name in os.environ:
-    result = os.environ[envPrefix+name]
-  return result
-    
+def read_configuration_file(filename=CONFIG_FILENAME):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except (OSError, ValueError):
+        return {}
 
-def ReadMultiSetting(name):
-  result = None
-  try:
-    with open(config) as data_file:    
-      data = json.load(data_file)
-    result = { key:data[key] for key in filter(lambda x: x.startswith(name), data.keys()) }
-  except (FileNotFoundError):
-    pass
 
-  result = result if result else {}
-  
-  prefix = envPrefix + name
-  envResult = { NormalizePrefix(name,key):os.environ[key] for key in filter(lambda x: x.lower().startswith(prefix.lower()), list(os.environ.keys())) }
-
-  finalResult = list({**result,**envResult}.values())
-  finalResult = finalResult if len(finalResult) else None
-  return finalResult
-
-def ReadSetting(name):
-  if name.endswith("*"):
-    return ReadMultiSetting(name[0:-1])
-  else:
-    return ReadSingleSetting(name)   
-
-PrintWithTimestamp(f"vault-unseal.py version {version}")
-
-if "VU_DEBUG" in os.environ:
-  PrintDebug("Dumping environment block:")
-  pprint(dict(os.environ))
-
-addressUrl = ReadSetting("ADDRESS_URL")
-timeIntervalSeconds = int(ReadSetting("TIME_INTERVAL_SECONDS"))
-unsealKeys = ReadSetting("UNSEAL_KEY_*")
-
-if not addressUrl:
-  PrintParameterError("ADDRESS_URL")
-  sys.exit(1)
-
-if not timeIntervalSeconds:
-  PrintParameterError("TIME_INTERVAL_SECONDS")
-  sys.exit(1)
-
-if not unsealKeys:
-  PrintParameterError("UNSEAL_KEY_*")
-  sys.exit(1)
-
-PrintWithTimestamp(f"ADDRESS_URL = {addressUrl}")
-PrintWithTimestamp(f"TIME_INTERVAL_SECONDS = {timeIntervalSeconds}")
-PrintWithTimestamp("Number of unseal keys: " + str(len(unsealKeys)))
-
-PrintDebug("UNSEAL_KEYS:")
-for key in unsealKeys:
-  PrintDebug(f"- {key}")
-PrintWithTimestamp("If you do not see any output below, it means that the vault is contacted successfully and its unsealed")
-PrintWithTimestamp(f"Vault will be contacted every {timeIntervalSeconds} seconds")
-PrintWithTimestamp("Run with environment variable VU_DEBUG set to 1 for debug output")
-
-while True:
-  try:
-    r = requests.get(f"{addressUrl}/v1/sys/seal-status").json()
-    PrintDebug(f"status:{r}")
-    if "sealed" in r:
-      if r["sealed"] == True:
-        PrintWithTimestamp("Detected sealed vault. Unsealing...")
-        for key in unsealKeys:
-          PrintDebug(f"key:{key}")
-          r = requests.put(f"{addressUrl}/v1/sys/unseal", json = {"key":key}).json()
-          PrintDebug(f"unseal:{r}")
-        if r["sealed"] == True:
-          PrintWithTimestamp("something went wrong, failed to unseal. Check the keys")
-          PrintWithTimestamp(r)
-          sys.exit(2)
-        else:
-          PrintWithTimestamp("Unsealed successfully")
+def read_setting(setting_name, parameters, logger):
+    if setting_name.endswith('*'):
+        prefix = setting_name[:-1]
+        setting = {key: value for key, value in parameters.items() if key.startswith(prefix)}
     else:
-      PrintWithTimestamp("Error: cannot find 'sealed' in returned json")
-      pprint(r)
-  except Exception as e:
-    PrintWithTimestamp(f"Exception:{e}")
-    PrintWithTimestamp(type(e))
-    pprint(vars(e))
-  time.sleep(timeIntervalSeconds)  
+        setting = parameters.get(setting_name)
+
+    if not setting:
+        message = 'Error: %s is not specified. Specify it either in %s or as environment variable prefixed with %s'
+        logger.error(message, setting_name, CONFIG_FILENAME, ENV_PREFIX)
+        sys.exit(1)
+
+    return setting
+
+
+def unseal(base_url, time_interval, unseal_keys, logger):
+    url = f'{base_url}/v1/sys/seal-status'
+    unseal_url = f'{base_url}/v1/sys/unseal'
+
+    while True:
+        try:
+            r = requests.get(url).json()
+            logger.debug('status: %s', r)
+
+            try:
+                sealed = r['sealed']
+            except KeyError:
+                logger.error('Error: cannot find \'sealed\' in returned JSON\n%s', r)
+            else:
+                if sealed:
+                    logger.info('Detected sealed vault. Unsealing...')
+
+                    for key_name, key_value in unseal_keys.items():
+                        logger.debug('Using key %s (%s)', key_name, key_value)
+                        r = requests.put(unseal_url, json={'key': key_value}).json()
+                        if r['sealed']:
+                            logger.debug('Unseal result: %s', r)
+                        else:
+                            logger.info('Unsealed successfully')
+                            break
+                    else:
+                        logger.error('Something went wrong, failed to unseal. Check the keys.\n%s', r)
+                        sys.exit(2)
+        except Exception:
+            logger.exception('An exception occured:')
+        time.sleep(time_interval)
+
+
+def main():
+    logger = configure_logger()
+    logger.info('vault-unseal.py version %s', VERSION)
+    logger.debug('Dumping environment block: \n%s', pformat(dict(os.environ)))
+
+    # Filter environment variables of interest
+    settings = {key[len(ENV_PREFIX):]: value for key, value in os.environ.items() if key.startswith(ENV_PREFIX)}
+    # Update values with those found in configuration file
+    settings.update(read_configuration_file())
+
+    # Retrieve required parameters
+    address_url = read_setting('ADDRESS_URL', settings, logger)
+    time_interval = int(read_setting('TIME_INTERVAL_SECONDS', settings, logger))
+    unseal_keys = read_setting('UNSEAL_KEY_*', settings, logger)
+
+    logger.info('ADDRESS_URL = %s', address_url)
+    logger.info('TIME_INTERVAL_SECONDS = %d', time_interval)
+    logger.info('Number of unseal keys: %d', len(unseal_keys))
+    logger.debug('UNSEAL_KEYS:')
+    for key, value in unseal_keys.items():
+        logger.debug('- %s: %s', key, value)
+
+    logger.info('If you do not see any output below, it means that the vault is contacted successfully and its unsealed')
+    logger.info('Vault will be contacted every %d seconds', time_interval)
+    logger.info('Run with environment variable VU_DEBUG set for debug output')
+
+    unseal(address_url, time_interval, unseal_keys, logger)
+
+
+if __name__ == '__main__':
+    main()
